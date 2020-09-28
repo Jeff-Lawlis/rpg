11e329247f57881de56dd317611fbfe71d8e49b7
Jeff Lawlis <jeff.lawlis@yahoo.com>
2020-09-28 15:03:10 -0500

    added select opponent render

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -29,6 +29,8 @@ var characters = [
     }
 ]
 
+var playerChar
+
 function populate(){
     document.getElementById("char-container").innerHTML = `
         <h2>CHOOSE YOUR FIGHTER</h2>
@@ -46,17 +48,48 @@ function populate(){
     
 }
 function chooseCharacter(){
-    var playerChar
+    // var playerChar
     for (var i=0; i<document.getElementsByClassName("single-char").length; i++){
         document.getElementById("char-div"+i).addEventListener("click", function(e){
             playerChar = characters[this.id[this.id.length-1]]
             console.log(playerChar)
+            renderPlayerChosen()
         })
     }
 }
+function renderPlayerChosen(){
+    var oppRemaining = []
+    document.getElementById("char-container").innerHTML = `
+        <h2>CHOOSE YOUR OPPONENT</h2>
+        <div id="player-char" class="single-char">
+            <span>${playerChar.name}</span>
+            <img src="${playerChar.imgUrl}"/>
+            <span>${playerChar.hp}</span>
+        </div>
+        <div id="opp-chars">
+        </div>
+    `
+    for(var i=0; i<characters.length; i++){
+        if(characters[i].name !== playerChar.name){
+            oppRemaining.push(characters[i])
+            var charDiv = `
+            <div id=char-div${i} class=single-char>
+                <span>${characters[i].name}</span>
+                <img src="${characters[i].imgUrl}"/>
+                <span>${characters[i].hp}</span>
+            </div>
+        `
+        }
+        console.log(oppRemaining);
+        
+        document.getElementById("opp-chars").innerHTML += charDiv
+    }
+
+    
+
+}
 populate()
 chooseCharacter()
-
 
 
 
